ba6c00d5b2e299227063dc66cea054038b58c758
kigawa <kigawa.8390@gmail.com>
2023-12-06 03:57:08 +0900

    add: detail page

--- mcwall/admin.py
+++ mcwall/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from mcwall.models import ImageModel
+
 # Register your models here.
+admin.site.register(ImageModel)
--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import UserManager, PermissionsMixin
 from django.db import models
 
 
@@ -9,9 +9,11 @@ class AppUserManager(UserManager):
     pass
 
 
-class AppUser(AbstractBaseUser):
+class AppUser(AbstractBaseUser, PermissionsMixin):
     uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     username = models.CharField(max_length=150, blank=False, unique=True)
     is_superuser = models.BooleanField(default=False)
     USERNAME_FIELD = 'username'
     objects = AppUserManager()
+    email = models.EmailField(blank=True)
+    is_staff = models.BooleanField(default=False)
